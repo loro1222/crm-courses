b1c23551c47b19c97bc726ed7bb0a96b84c852cb
Skward18 <elbek1987.26.07@mail.ru>
2022-10-30 17:55:46 +0500

    Initial commit

--- src/com/demo/crm/model/Mentor.java
+++ src/com/demo/crm/model/Mentor.java
@@ -4,29 +4,25 @@ import java.time.LocalDate;
 
 public class Mentor extends Person {
 
-    private Double salary;
-
-    public Mentor() {
+   public Mentor() {
 
     }
 
     public Mentor(Long id, String firstName, String lastName, String email, String phoneNumber, LocalDate dob, Double salary) {
         super(id, firstName, lastName, email, phoneNumber, dob);
-        this.salary = salary;
-    }
 
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
     }
 
     @Override
     public String toString() {
         return "Mentor{" +
-                "salary=" + salary +
+                " id = " + getId() +
+                ", firstName = '" + getFirstName() + '\'' +
+                ", lastName = '" + getLastName() + '\'' +
+                ", email = '" + getEmail() + '\'' +
+                ", phoneNumber = '" + getPhoneNumber() + '\'' +
+                ", dob = " + getDob() +
+                ", createDate = " + getCreateDate() +
                 '}';
     }
 }
