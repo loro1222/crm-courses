8cf3f142470e4fde2c996cb22e2d2e6d6837b28b
NodirUmarov <mr.umarov.1995@gmail.com>
2022-10-31 21:02:34 +0500

    Student service create method

---
+++ src/com/demo/crm/Main.java
@@ -0,0 +1,17 @@
+package com.demo.crm;
+
+import com.demo.crm.model.Student;
+import com.demo.crm.service.StudentService;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentService studentService = new StudentService(); //
+        Student student = studentService.create();
+
+        StudentService studentService1 = new StudentService();
+        Student student1 = studentService1.create();
+
+        System.out.println(student);
+        System.out.println(student1);
+    }
+}
--- src/com/demo/crm/service/StudentService.java
+++ src/com/demo/crm/service/StudentService.java
@@ -1,7 +1,55 @@
 package com.demo.crm.service;
 
+import com.demo.crm.model.Student;
+import com.demo.crm.model.builder.StudentBuilder;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class StudentService {
 
+    private Scanner scanner;
+    private static Long idCounter;
 
+    static {
+        System.out.println("Static block initialized");
+        idCounter = 0L;
+    }
+
+    public StudentService() {
+        System.out.println("Student service constructor invoked");
+        this.scanner = new Scanner(System.in);
+    }
+
+    public Student create() {
+        System.out.println("Creating student...");
+
+        System.out.println("Please, enter student data below.");
+        System.out.print("First name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Phone number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Date of birth(dd.MM.yyyy): ");
+        String dobInString = scanner.nextLine();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dob = LocalDate.parse(dobInString, formatter);
+
+        return StudentBuilder.builder().id(++idCounter)
+                .firstName(firstName)
+                .lastName(lastName)
+                .email(email)
+                .phoneNumber(phoneNumber)
+                .dob(dob).build();
+    }
 
 }
